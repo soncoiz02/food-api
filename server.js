494c950fa76133a29a8bb124aa9107239a9f8481
soncoiz02 <soncoiz3107@gmail.com>
2023-07-10 00:51:32 +0700

    feat: update db

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
+
+server.db = router.db
 
 server.use(middlewares);
 // Add this before server.use(router)
@@ -10,10 +13,13 @@ server.use(
 	// Add custom route here if needed
 	jsonServer.rewriter({
 		"/api/*": "/$1",
+		"/users*": "/600/users$1"
 	})
 );
+
+server.use(auth)
 server.use(router);
-server.listen(3000, () => {
+server.listen(3001, () => {
 	console.log("JSON Server is running");
 });
 
